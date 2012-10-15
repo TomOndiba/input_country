0df2c205b1bdc054c5c462e682b8361e62211635
Marcus Povey <marcus@marcus-povey.co.uk>
2012-10-15 10:11:13 +0100

    Handling status codes

--- views/default/output/country.php
+++ views/default/output/country.php
@@ -4,5 +4,5 @@
 	require_once("{$dir}input_country/vendor/phpCountryDropdown/phpCountryDropdown.lib.php");
 	
 	$countries = getCountryArray();
-
-	echo htmlspecialchars($vars['value'], ENT_QUOTES, 'UTF-8', false);
+	
+	echo htmlspecialchars($countries[$vars['value']], ENT_QUOTES, 'UTF-8', false);
--- views/default/output/stateus.php
+++ views/default/output/stateus.php
@@ -3,6 +3,6 @@
 	$dir = elgg_get_plugins_path();
 	require_once("{$dir}input_country/vendor/phpCountryDropdown/phpCountryDropdown.lib.php");
 	
-	$statesus = getUSStatesArray();
-
-	echo htmlspecialchars($vars['value'], ENT_QUOTES, 'UTF-8', false);
+	$states = getUSStatesArray();
+	
+	echo htmlspecialchars($states[$vars['value']], ENT_QUOTES, 'UTF-8', false);
